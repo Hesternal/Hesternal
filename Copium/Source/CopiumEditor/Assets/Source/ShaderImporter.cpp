a54968fbf822bab9dca3414f5fbeff850256dd47
Vladimir Matushkin <matushkin.vladimir98@gmail.com>
2022-08-01 11:43:45 +0500

    Add ShaderImporter class

---
+++ Copium/Source/CopiumEditor/Assets/Source/ShaderImporter.cpp
@@ -0,0 +1,34 @@
+module;
+
+#include "Engine/Core/Debug.hpp"
+
+module CopiumEditor.Assets.ShaderImporter;
+
+import CopiumEngine.Assets.Shader;
+import CopiumEngine.Core.Platform.File;
+
+import CopiumEditor.Assets.ShaderImporter.ParserError;
+import CopiumEditor.Assets.ShaderImporter.ShaderParser;
+
+import <string>;
+
+
+namespace Copium
+{
+
+    Shader ShaderImporter::Import(const std::string& shaderPath)
+    {
+        const std::string shaderFileContent = File::ReadAllText(shaderPath);
+
+        try
+        {
+            return Shader(ShaderParser::Parse(shaderFileContent));
+        }
+        catch (const ParserError& e)
+        {
+            COP_LOG_CRITICAL(e.Message);
+            throw;
+        }
+    }
+
+} // namespace Copium
