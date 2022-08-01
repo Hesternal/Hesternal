77d52de66f24a44b5f545788891cca7c46845f91
Vladimir Matushkin <matushkin.vladimir98@gmail.com>
2022-08-01 15:16:20 +0500

    Create window with the correct size; Remove Windows.h include from Window module

--- Copium/Source/CopiumEngine/Platform/Public/Window.ixx
+++ Copium/Source/CopiumEngine/Platform/Public/Window.ixx
@@ -1,20 +1,11 @@
-module;
-
-#include "Engine/Core/Defines.hpp"
-
-// TODO(v.matushkin): Probably shouldn't leak windows.h like this, although it's modules now, idk how much damage it does
-#include <Windows.h>
-
-// NOTE(v.matushkin): <SAL Warnings> May be it will be fixed once we get import std;
-COP_WARNING_PUSH
-COP_WARNING_DISABLE_MSVC(4005) // warning C4005: macro redefinition
-COP_WARNING_DISABLE_MSVC(5106) // warning C5106: macro redefined with different parameter names
 export module CopiumEngine.Platform.Window;
 
 import CopiumEngine.Core.CoreTypes;
 import CopiumEngine.Event.EventManager;
-import CopiumEngine.Graphics;
+import CopiumEngine.Graphics.GraphicsTypes;
 import CopiumEngine.Platform.PlatformTypes;
+
+import <memory>;
 
 
 export namespace Copium
@@ -22,9 +13,10 @@ export namespace Copium
 
     class Window final
     {
+        struct PlatformData;
+
     public:
         Window(WindowDesc&& windowDesc);
-COP_WARNING_POP
         ~Window();
 
         // [[nodiscard]] uint64 GetNativeHandle() { return reinterpret_cast<uint64>(m_hWnd); }
@@ -39,15 +31,11 @@ COP_WARNING_POP
         void OnWindowClose();
         void OnWindowResize(const WindowResizeEvent& evt);
 
-        static int64 __stdcall WndProc(HWND hWnd, uint32 uMsg, uint64 wParam, int64 lParam);
-
     private:
-        WindowDesc      m_desc;
-
-        HWND            m_hWnd;
-        SwapchainHandle m_swapchainHandle;
-
-        bool            m_isClosing;
+        WindowDesc                    m_desc;
+        std::unique_ptr<PlatformData> m_platformData;
+        SwapchainHandle               m_swapchainHandle;
+        bool                          m_isClosing;
     };
 
 } // export namespace Copium
--- Copium/Source/CopiumEngine/Platform/Source/Windows/Window.cpp
+++ Copium/Source/CopiumEngine/Platform/Source/Windows/Window.cpp
@@ -12,6 +12,7 @@ module CopiumEngine.Platform.Window;
 COP_WARNING_POP
 
 import CopiumEngine.Event.EventManager;
+import CopiumEngine.Graphics;
 
 import <utility>;
 
@@ -21,114 +22,16 @@ namespace
 
     using namespace Copium;
 
-    static const wchar_t* k_ClassName = L"CopiumWindowClass";
 
-    static const uint8               k_SwapchainBufferCount = 3;
-    static const RenderTextureFormat k_SwapchainFormat      = RenderTextureFormat::BGRA8;
+    static constexpr const wchar_t* k_ClassName = L"CopiumWindowClass";
 
-} // namespace
-
-
-namespace Copium
-{
-
-    Window::Window(WindowDesc&& windowDesc)
-        : m_desc(std::move(windowDesc))
-        , m_isClosing(false)
-    {
-        HINSTANCE hInstance = GetModuleHandleW(nullptr);
-
-        WNDCLASSEXW wndClassEx = {
-            .cbSize        = sizeof(WNDCLASSEXW),
-            .style         = CS_DBLCLKS,
-            .lpfnWndProc   = WndProc,
-            .cbClsExtra    = 0,
-            .cbWndExtra    = 0,
-            .hInstance     = hInstance,
-            .hIcon         = nullptr,
-            .hCursor       = LoadCursorW(nullptr, IDC_ARROW),
-            .hbrBackground = nullptr, // static_cast<HBRUSH>(GetStockObject(BLACK_BRUSH)),
-            .lpszMenuName  = nullptr,
-            .lpszClassName = k_ClassName,
-            .hIconSm       = nullptr,
-        };
-        ATOM classUniqueId = RegisterClassExW(&wndClassEx);
-        COP_ASSERT(classUniqueId != 0);
-
-        // https://docs.microsoft.com/en-us/windows/win32/winmsg/window-styles
-        // https://docs.microsoft.com/en-us/windows/win32/winmsg/extended-window-styles
-        DWORD dwExStyle = 0;
-        DWORD dwStyle = WS_VISIBLE | WS_OVERLAPPEDWINDOW;// | WS_CAPTION | WS_SYSMENU;
-
-        m_hWnd = CreateWindowExW(
-            dwExStyle,
-            k_ClassName,
-            windowDesc.Title.c_str(),
-            dwStyle,
-            CW_USEDEFAULT, CW_USEDEFAULT,
-            windowDesc.Width, windowDesc.Height,
-            nullptr,
-            nullptr,
-            hInstance,
-            nullptr
-        );
-        COP_ASSERT(m_hWnd != nullptr);
-
-        SwapchainDesc swapchainDesc = {
-            .WindowNativeHandle = reinterpret_cast<uint64>(m_hWnd),
-            .Width              = m_desc.Width,
-            .Height             = m_desc.Height,
-            .BufferCount        = k_SwapchainBufferCount,
-            .Format             = k_SwapchainFormat,
-        };
-        m_swapchainHandle = Graphics::GetGraphicsDevice()->CreateSwapchain(swapchainDesc);
-
-        WindowHandle windowHandle = static_cast<WindowHandle>(reinterpret_cast<uint64>(m_hWnd));
-        EventManager::RegisterWindowCloseCallback(windowHandle, [this]() { OnWindowClose(); });
-        EventManager::RegisterWindowResizeCallback(windowHandle, [this](const WindowResizeEvent& evt) { OnWindowResize(evt); });
-    }
-
-    Window::~Window()
-    {
-        Graphics::GetGraphicsDevice()->DestroySwapchain(m_swapchainHandle);
-        DestroyWindow(m_hWnd);
-        // UnregisterClassW();
-    }
+    static constexpr uint8               k_SwapchainBufferCount = 3;
+    static constexpr RenderTextureFormat k_SwapchainFormat      = RenderTextureFormat::BGRA8;
 
 
-    void Window::ProcessEvents()
-    {
-        MSG msg;
-
-        while (PeekMessageW(&msg, nullptr, 0, 0, PM_REMOVE) != 0)
-        {
-            // if (msg.message == WM_QUIT)
-            // {
-            //     // TODO(v.matushkin): Handle
-            // }
-
-            TranslateMessage(&msg);
-            DispatchMessageW(&msg);
-        }
-    }
-
-
-    void Window::OnWindowClose()
-    {
-        m_isClosing = true;
-    }
-
-    void Window::OnWindowResize(const WindowResizeEvent& evt)
-    {
-        COP_LOG_INFO("Resizing swapchain, width: {:d} | height: {:d}", evt.Width, evt.Height);
-        Graphics::GetGraphicsDevice()->ResizeSwapchain(m_swapchainHandle, evt.Width, evt.Height);
-    }
-
-
-    int64 __stdcall Window::WndProc(HWND hWnd, uint32 uMsg, uint64 wParam, int64 lParam)
+    static int64 __stdcall WndProc(HWND hWnd, uint32 uMsg, uint64 wParam, int64 lParam)
     {
         // NOTE(v.matushkin): Is there a way to handle events for no yet created window?
-
         WindowHandle windowHandle = static_cast<WindowHandle>(reinterpret_cast<uint64>(hWnd));
 
         switch (uMsg)
@@ -167,6 +70,128 @@ namespace Copium
         };
 
         return DefWindowProcW(hWnd, uMsg, wParam, lParam);
+    }
+
+} // namespace
+
+
+namespace Copium
+{
+
+    struct Window::PlatformData
+    {
+        HWND Window;
+    };
+
+
+    Window::Window(WindowDesc&& windowDesc)
+        : m_desc(std::move(windowDesc))
+        , m_isClosing(false)
+    {
+        PlatformData platformData;
+
+        const HINSTANCE hInstance = GetModuleHandleW(nullptr);
+
+        //- Register Window class
+        const WNDCLASSEXW wndClassEx = {
+            .cbSize        = sizeof(WNDCLASSEXW),
+            .style         = CS_DBLCLKS, // Capture double-clicks
+            .lpfnWndProc   = WndProc,
+            .cbClsExtra    = 0,
+            .cbWndExtra    = 0,
+            .hInstance     = hInstance,
+            .hIcon         = nullptr,
+            .hCursor       = LoadCursorW(nullptr, IDC_ARROW),
+            .hbrBackground = nullptr, // static_cast<HBRUSH>(GetStockObject(BLACK_BRUSH)),
+            .lpszMenuName  = nullptr,
+            .lpszClassName = k_ClassName,
+            .hIconSm       = nullptr,
+        };
+        const ATOM classUniqueId = RegisterClassExW(&wndClassEx);
+        COP_ASSERT(classUniqueId != 0);
+
+        //- Create Window
+        {
+            // https://docs.microsoft.com/en-us/windows/win32/winmsg/window-styles
+            // https://docs.microsoft.com/en-us/windows/win32/winmsg/extended-window-styles
+            const DWORD dwStyle =
+                WS_VISIBLE             // The window is initially visible
+                | WS_OVERLAPPEDWINDOW; // WS_OVERLAPPED | WS_CAPTION | WS_SYSMENU | WS_THICKFRAME | WS_MINIMIZEBOX | WS_MAXIMIZEBOX
+            const DWORD dwExStyle = 0;
+
+            //-- Adjust window size
+            RECT borderRect{};
+            // borderRect will be something like this: left -8 | right 8 | top -31 | bottom 8
+            AdjustWindowRectEx(&borderRect, dwStyle, false, dwExStyle);
+
+            const uint32 windowWidth = windowDesc.Width + borderRect.right - borderRect.left;
+            const uint32 windowHeight = windowDesc.Height + borderRect.bottom - borderRect.top;
+
+            platformData.Window = CreateWindowExW(
+                dwExStyle,
+                k_ClassName,
+                windowDesc.Title.c_str(),
+                dwStyle,
+                CW_USEDEFAULT, CW_USEDEFAULT,
+                windowWidth, windowHeight,
+                nullptr,
+                nullptr,
+                hInstance,
+                nullptr
+            );
+            COP_ASSERT(platformData.Window != nullptr);
+        }
+
+        SwapchainDesc swapchainDesc = {
+            .WindowNativeHandle = reinterpret_cast<uint64>(platformData.Window),
+            .Width              = m_desc.Width,
+            .Height             = m_desc.Height,
+            .BufferCount        = k_SwapchainBufferCount,
+            .Format             = k_SwapchainFormat,
+        };
+        m_swapchainHandle = Graphics::GetGraphicsDevice()->CreateSwapchain(swapchainDesc);
+
+        m_platformData = std::make_unique<PlatformData>(platformData);
+
+        WindowHandle windowHandle = static_cast<WindowHandle>(reinterpret_cast<uint64>(platformData.Window));
+        EventManager::RegisterWindowCloseCallback(windowHandle, [this]() { OnWindowClose(); });
+        EventManager::RegisterWindowResizeCallback(windowHandle, [this](const WindowResizeEvent& evt) { OnWindowResize(evt); });
+    }
+
+    Window::~Window()
+    {
+        Graphics::GetGraphicsDevice()->DestroySwapchain(m_swapchainHandle);
+        DestroyWindow(m_platformData->Window);
+        // UnregisterClassW();
+    }
+
+
+    void Window::ProcessEvents()
+    {
+        MSG msg;
+
+        while (PeekMessageW(&msg, nullptr, 0, 0, PM_REMOVE) != 0)
+        {
+            // if (msg.message == WM_QUIT)
+            // {
+            //     // TODO(v.matushkin): Handle
+            // }
+
+            TranslateMessage(&msg);
+            DispatchMessageW(&msg);
+        }
+    }
+
+
+    void Window::OnWindowClose()
+    {
+        m_isClosing = true;
+    }
+
+    void Window::OnWindowResize(const WindowResizeEvent& evt)
+    {
+        COP_LOG_INFO("Resizing swapchain, width: {:d} | height: {:d}", evt.Width, evt.Height);
+        Graphics::GetGraphicsDevice()->ResizeSwapchain(m_swapchainHandle, evt.Width, evt.Height);
     }
 
 } // namespace Copium
