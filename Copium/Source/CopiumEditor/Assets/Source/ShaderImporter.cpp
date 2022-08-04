210b31c21caf8dbd927984378d4494056bf8e272
Vladimir Matushkin <matushkin.vladimir98@gmail.com>
2022-08-04 13:28:19 +0500

    Add DX11ShaderCompiler (#27)

--- Copium/Source/CopiumEditor/Assets/Source/ShaderImporter.cpp
+++ Copium/Source/CopiumEditor/Assets/Source/ShaderImporter.cpp
@@ -7,10 +7,13 @@ module CopiumEditor.Assets.ShaderImporter;
 import CopiumEngine.Assets.Shader;
 import CopiumEngine.Core.Platform.File;
 
+import CopiumEditor.Assets.DX11ShaderCompiler;
+import CopiumEditor.Assets.ParsedShaderDesc;
 import CopiumEditor.Assets.ShaderImporter.ParserError;
 import CopiumEditor.Assets.ShaderImporter.ShaderParser;
 
 import <string>;
+import <utility>;
 
 
 namespace Copium
@@ -22,7 +25,19 @@ namespace Copium
 
         try
         {
-            return Shader(ShaderParser::Parse(shaderFileContent));
+            ParsedShaderDesc parsedShaderDesc = ShaderParser::Parse(shaderFileContent);
+            ShaderDesc shaderDesc = {
+                .RasterizerStateDesc   = parsedShaderDesc.RasterizerStateDesc,
+                .DepthStencilStateDesc = parsedShaderDesc.DepthStencilStateDesc,
+                .BlendStateDesc        = parsedShaderDesc.BlendStateDesc,
+            };
+
+            DX11ShaderCompiler dx11ShaderCompiler;
+            dx11ShaderCompiler.Compile(parsedShaderDesc, shaderDesc);
+
+            shaderDesc.Name = std::move(parsedShaderDesc.Name);
+
+            return Shader(std::move(shaderDesc));
         }
         catch ([[maybe_unused]] const ParserError& e)
         {
