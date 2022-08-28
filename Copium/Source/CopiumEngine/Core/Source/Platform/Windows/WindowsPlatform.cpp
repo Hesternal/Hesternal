f1759d05459734130622fd4a4246aa5987315cce
Vladimir Matushkin <matushkin.vladimir98@gmail.com>
2022-08-28 12:45:28 +0500

    WindowsPlatform: add string to wstring conversion function

--- Copium/Source/CopiumEngine/Core/Source/Platform/Windows/WindowsPlatform.cpp
+++ Copium/Source/CopiumEngine/Core/Source/Platform/Windows/WindowsPlatform.cpp
@@ -3,6 +3,7 @@ module;
 #include "Engine/Core/Defines.hpp"
 
 #include <combaseapi.h>
+#include <Windows.h>
 
 // NOTE(v.matushkin): <SAL Warnings> May be it will be fixed once we get import std;
 COP_WARNING_PUSH
@@ -10,6 +11,7 @@ COP_WARNING_DISABLE_MSVC(4005) // warning C4005: macro redefinition
 COP_WARNING_DISABLE_MSVC(5106) // warning C5106: macro redefined with different parameter names
 module CopiumEngine.Core.Platform;
 
+import CopiumEngine.Core.CoreTypes;
 import CopiumEngine.Core.Misc.Guid;
 COP_WARNING_POP
 
@@ -25,6 +27,22 @@ namespace Copium
     void Platform::GenerateGuid(Guid& guid)
     {
         CoCreateGuid(reinterpret_cast<GUID*>(&guid));
+    }
+
+    std::wstring Platform::ToWideString(std::string_view str)
+    {
+        std::wstring wideString;
+
+        const int32 stringLength = static_cast<int32>(str.length());
+        const int32 wideStringLength = MultiByteToWideChar(65001 /*CP_UTF8*/, 0, str.data(), stringLength, nullptr, 0);
+
+        if (wideStringLength != 0)
+        {
+            wideString.resize(wideStringLength);
+            MultiByteToWideChar(65001 /*CP_UTF8*/, 0, str.data(), stringLength, wideString.data(), wideStringLength);
+        }
+
+        return wideString;
     }
 
 } // namespace Copium
