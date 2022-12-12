22afddda5fc9002d0ef059d482f9057cc9cea115
Vladimir Matushkin <matushkin.vladimir98@gmail.com>
2022-12-12 19:27:35 +0600

    Add TransformHierarchySystem

--- Copium/Source/CopiumEngine/ECS/Source/WorldManager.cpp
+++ Copium/Source/CopiumEngine/ECS/Source/WorldManager.cpp
@@ -4,6 +4,7 @@ import CopiumEngine.ECS.System.Camera;
 import CopiumEngine.ECS.System.Input;
 import CopiumEngine.ECS.System.Graphics;
 import CopiumEngine.ECS.System.TransformToMatrix;
+import CopiumEngine.ECS.System.TransformHierarchy;
 
 import <memory>;
 import <vector>;
@@ -20,6 +21,7 @@ namespace Copium
         std::vector<std::unique_ptr<ISystem>> systems;
         systems.push_back(std::make_unique<InputSystem>());
         systems.push_back(std::make_unique<TransformToMatrixSystem>());
+        systems.push_back(std::make_unique<TransformHierarchySystem>());
         systems.push_back(std::make_unique<CameraSystem>());
         systems.push_back(std::make_unique<GraphicsSystem>());
 
