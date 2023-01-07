58e654478a75f2a5753185ac9faed12877d66c26
Vladimir Matushkin <matushkin.vladimir98@gmail.com>
2023-01-07 21:56:23 +0600

    Rename Hesternal.HeaderTool.Program -> Hesternal.HeaderTool.App

--- generate-vs.ps1
+++ generate-vs.ps1
@@ -1,5 +1,5 @@
 dotnet build --configuration Release .\Hesternal\Source\Tools\HesternalTools.sln
 dotnet publish -p:PublishProfile=FolderProfile .\Hesternal\Source\Tools\Hesternal.BuildTool.App\Hesternal.BuildTool.App.csproj
-dotnet publish -p:PublishProfile=FolderProfile .\Hesternal\Source\Tools\Hesternal.HeaderTool.Program\Hesternal.HeaderTool.Program.csproj
+dotnet publish -p:PublishProfile=FolderProfile .\Hesternal\Source\Tools\Hesternal.HeaderTool.App\Hesternal.HeaderTool.App.csproj
 
 .\build\bin\dotnet\HesternalBuildTool.exe generate --project-dir=".\Hesternal\Source\"
