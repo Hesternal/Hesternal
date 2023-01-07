cb77aec3d81abd3ecef876dd5f5b73be5791cb32
Vladimir Matushkin <matushkin.vladimir98@gmail.com>
2023-01-07 16:49:24 +0600

    Transfer: rename Programs/CopiumBuildTool folder

--- generate-vs.ps1
+++ generate-vs.ps1
@@ -1,5 +1,5 @@
-dotnet build --configuration Release .\Copium\Source\Programs\CopiumBuildTool\HesternalBuildTool.sln
-dotnet publish -p:PublishProfile=FolderProfile .\Copium\Source\Programs\CopiumBuildTool\Hesternal.BuildTool.App\Hesternal.BuildTool.App.csproj
-dotnet publish -p:PublishProfile=FolderProfile .\Copium\Source\Programs\CopiumBuildTool\Hesternal.HeaderTool.Program\Hesternal.HeaderTool.Program.csproj
+dotnet build --configuration Release .\Copium\Source\Programs\HesternalBuildTool\HesternalBuildTool.sln
+dotnet publish -p:PublishProfile=FolderProfile .\Copium\Source\Programs\HesternalBuildTool\Hesternal.BuildTool.App\Hesternal.BuildTool.App.csproj
+dotnet publish -p:PublishProfile=FolderProfile .\Copium\Source\Programs\HesternalBuildTool\Hesternal.HeaderTool.Program\Hesternal.HeaderTool.Program.csproj
 
 .\build\bin\dotnet\HesternalBuildTool.exe generate --project-dir=".\Copium\Source\"
